8f364eed9c99ddf2585bf6ba831ca48f3cde92e1
Marcelo <cheloz_20@hotmail.com>
2019-08-07 16:56:41 -0400

    Saludo basico

---
+++ src/algo_basico/Algo_basico.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package algo_basico;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Chelo
+ */
+public class Algo_basico {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Nombre?");
+        Scanner s = new Scanner(System.in);
+        String msg=s.nextLine();
+        System.out.println("Hola "+msg);
+        
+        
+    }
+    
+}
